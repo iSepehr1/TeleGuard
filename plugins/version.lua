297ab0933daa2db675b001d50831d9eab4e9b6ae
iSepehr2001 <iSepehr2001@users.noreply.github.com>
2016-01-25 16:09:58 +0330

    Create version.lua

---
+++ plugins/version.lua
@@ -0,0 +1,29 @@
+do
+
+function run(msg, matches)
+  return 'Teleguard v1'.. [[ 
+  
+  An advance Administration bot based on yagop/telegram-bot
+
+http://s6.uplod.ir/i/00729/d0pl8h54fjbd.jpg
+
+
+Admins
+@iSepehr2001
+@ihossein7
+
+
+Our Support Bot
+@TeleGuardHelp_Bot
+  end
+
+return {
+  description = "Shows bot version", 
+  usage = "!teleguard: Shows bot version",
+  patterns = {
+    "^teleguard$"
+  }, 
+  run = run 
+}
+
+end

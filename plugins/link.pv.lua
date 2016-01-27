fbc197d4543128e1854fe0b9de5d020326396225
iSepehr2001 <iSepehr2001@users.noreply.github.com>
2016-01-28 02:03:25 +0330

    Update link.pv.lua

--- plugins/link.pv.lua
+++ plugins/link.pv.lua
@@ -731,18 +731,19 @@ local function run(msg, matches)
     end
     if matches[1] == 'linkpv' then
       if not is_momod(msg) then
-        return "For moderators only!"
+        return "برای شما مجاز نیست"
       end
+      
       local group_link = data[tostring(msg.to.id)]['settings']['set_link']
       if not group_link then 
-        return "Create a link using /newlink first !"
+        return "ابتدا با دستور newlink لینک را بسازید"
       end
        savelog(msg.to.id, name_log.." ["..msg.from.id.."] requested group link ["..group_link.."]")
-     send_large_msg('user#id'..msg.from.id, "Group link:\n"..group_link)
+     send_large_msg('user#id'..msg.from.id, "لینک ورود به گروه:\n"..group_link)
     end
     if matches[1] == 'setowner' then
       if not is_owner(msg) then
-        return "For owner only!"
+        return "فقط برای صاحب اصلی گروه مجاز است"
       end
       data[tostring(msg.to.id)]['set_owner'] = matches[2]
       save_data(_config.moderation.data, data)
@@ -822,7 +823,7 @@ local function run(msg, matches)
       if not is_momod(msg) then
         return
       end
-      savelog(msg.to.id, name_log.." ["..msg.from.id.."] Used /help")
+      savelog(msg.to.id, name_log.." ["..msg.from.id.."] Used help")
       return help()
     end
     if matches[1] == 'res' and is_momod(msg) then 
@@ -831,14 +832,14 @@ local function run(msg, matches)
       }
       local username = matches[2]
       local username = username:gsub("@","")
-      savelog(msg.to.id, name_log.." ["..msg.from.id.."] Used /res "..username)
+      savelog(msg.to.id, name_log.." ["..msg.from.id.."] Used res "..username)
       return res_user(username,  callbackres, cbres_extra)
     end
   end 
 end
 return {
   patterns = {
-  "^[!/](linkpv)$",
+  "^[Ll]inkpv$",
   "%[(photo)%]",
   "^!!tgservice (.+)$",
   },
